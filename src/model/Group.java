12d802c208786283c979243c7e71450a5abbb06f
timeloveboy <734991033@qq.com>
2017-03-01 15:08:03 +0800

    ~

---
+++ src/model/Group.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Created by timeloveboy on 17-3-1.
+ */
+public class Group {
+    Integer Uid;
+    String NickName;
+    List<Integer> members;
+}
